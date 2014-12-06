9d579ffec83cfdcdd1c4ecedbb529375da20567b
李传岭 <lichuanling@lichuanlingdeMacBook-Pro.local>
2014-12-06 16:37:18 +0800

    add new project file

---
+++ HelloWorldApp/HelloWorldApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorldApp
+//
+//  Created by 李传岭 on 14-12-6.
+//  Copyright (c) 2014年 Lichuanling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorldApp/HelloWorldApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorldApp
+//
+//  Created by 李传岭 on 14-12-6.
+//  Copyright (c) 2014年 Lichuanling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
